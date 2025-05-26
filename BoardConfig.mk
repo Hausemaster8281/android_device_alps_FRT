c86775ad1ec806117e845c5396257b2c63eef358
Hausemaster8281 <66825701+Hausemaster8281@users.noreply.github.com>
2025-05-27 00:36:04 +0530

    Temporary: Disable AVB
    Disabled AVB to conduct initial testing

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,7 +118,7 @@ TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 BOARD_VNDK_VERSION := current
 
 # Verified Boot
-BOARD_AVB_ENABLE := true  # Disable for older devices
+BOARD_AVB_ENABLE := false  # Disable for older devices
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
 BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
